254f85da395ef27df5a901d2af81adcc2173d309
Eli293 <119442915+Eli293@users.noreply.github.com>
2022-12-21 11:11:36 -0500

    Backend Integration

---
+++ backend/BrainBubbleProject/src/main/java/com/example/brainbubbleproject/BrainBubbleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.brainbubbleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainBubbleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainBubbleProjectApplication.class, args);
+    }
+
+}
---
+++ backend/BrainBubbleProject/src/test/java/com/example/brainbubbleproject/BrainBubbleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brainbubbleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainBubbleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
